af49864d918c30916fb35d13eba6ef40f7ee83dc
mbaradza <mbaradza1@gmail.com>
2023-06-05 13:56:36 +0200

    ...

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 let config = {
      
     mongo: {
-        url: 'mongodb://127.0.0.1/PregnancyPrayer',
+        url: 'mongodb://127.0.0.1:27017/PregnancyPrayer',
         
     },
     BOT_API: process.env.BOT_KEY,
